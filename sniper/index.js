754007b6b16eef8e8f8afe906663fc55aab2aacf
zhouchuanfei <2162865811@qq.com>
2020-07-03 16:01:33 +0800

    代码

--- sniper/index.js
+++ sniper/index.js
@@ -1,16 +1,1 @@
-/**
- * 设置LayaNative屏幕方向，可设置以下值
- * landscape           横屏
- * portrait            竖屏
- * sensor_landscape    横屏(双方向)
- * sensor_portrait     竖屏(双方向)
- */
-window.screenOrientation = "sensor_landscape";
-
-//-----libs-begin-----
-loadLib("libs/min/laya.core.min.js")
-loadLib("libs/min/laya.ui.min.js")
-loadLib("libs/min/laya.d3.min.js")
-loadLib("libs/min/laya.physics3D.min.js")
-//-----libs-end-------
-loadLib("js/bundle.js");
+window.screenOrientation="sensor_landscape",loadLib("libs/min/laya.core.min.js"),loadLib("libs/min/laya.ui.min.js"),loadLib("libs/min/laya.d3.min.js"),loadLib("libs/min/laya.physics3D.min.js"),loadLib("js/bundle.js");
--- sniper/libs/laya.physics3D.runtime.js
+++ sniper/libs/laya.physics3D.runtime.js
@@ -1,10 +1,1 @@
-window.Physics3D = function(initialMemory, interactive) {
-	window.conch.setGetWorldTransformFunction(interactive.getWorldTransform);
-	window.conch.setSetWorldTransformFunction(interactive.setWorldTransform);
-	var conchBullet = window.layaConchBullet;
-	conchBullet.then = (complete) => {
-		complete();
-	};
-	window.Physics3D = conchBullet;
-	return conchBullet;
-};
+window.Physics3D=function(n,o){window.conch.setGetWorldTransformFunction(o.getWorldTransform),window.conch.setSetWorldTransformFunction(o.setWorldTransform);var r=window.layaConchBullet;return r.then=(n=>{n()}),window.Physics3D=r,r};
